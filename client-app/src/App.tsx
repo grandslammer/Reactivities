0263d9286f1eee4c2366e3b756c299cbfb35b95d
Ivan <ivanwhitemac@gmail.com>
2023-06-06 19:21:44 +0100

    Walking skeleton Client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button, Header, List } from "semantic-ui-react";
 import "./App.css";
 
 function App() {
 	const [activities, setActivities] = useState<any>([]);
 	useEffect(() => {
 		axios.get("http://localhost:5000/api/activities").then((response) => {
-			console.log(response);
+			// console.log(response);
 			setActivities(response.data);
 		});
 	}, []);
 
 	return (
 		<>
-			<ul>
+			<Header as="h2" icon="users" content="Reactivities" />
+			<List>
 				{activities.map((activity: any) => (
-					<li key={activity.id}>{activity.title}</li>
+					<List.Item key={activity.id}>{activity.title}</List.Item>
 				))}
-			</ul>
+			</List>
 		</>
 	);
 }
